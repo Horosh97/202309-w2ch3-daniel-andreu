ef611b53f9521158c7c2f84f77b6eb5022260d2c
Daniel Andreu <daniel1997ar@gmail.com>
2023-09-20 22:31:31 +0200

    Add filterSongByGenre function

--- index.js
+++ index.js
@@ -68,3 +68,8 @@ const deleteSong = (title, list) =>
   );
 
 const getSong = (title, list) => list.find((song) => song.title === title);
+
+const filterSongByGenre = (genre, list) =>
+  list.filter((song) => song.genre === genre);
+
+console.log(filterSongByGenre("Metal mestizaje", songs));
